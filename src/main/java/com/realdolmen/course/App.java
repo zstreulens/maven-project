40f8e5e5348f23d133c15e796a962f681ccae9fa
Ziggy Streulens <Ziggy.Streulens@realdolmen.com>
2016-08-05 11:37:52 +0200

    Files added + gitignore

---
+++ src/main/java/com/realdolmen/course/App.java
@@ -0,0 +1,13 @@
+package com.realdolmen.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
